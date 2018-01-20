69f489f0190470442367a905c61c5e688beaad76
Mikita Saladkevich <Mikita_Saladkevich@epam.com>
2018-01-20 16:19:23 +0300

    up to date

---
+++ src/main/go_lang_tour/conccurent/tree/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "github.com/tour/tree"
+import "fmt"
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Walker launches Walk in a new goroutine,
+// and returns a read-only channel of values.
+func Walker(t *tree.Tree) <-chan int {
+	ch := make(chan int)
+	go func() {
+		Walk(t, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+
+	c1, c2 := Walker(t1), Walker(t2)
+	for {
+		v1, ok1 := <-c1
+		v2, ok2 := <-c2
+		if !ok1 || !ok2 {
+			return ok1 == ok2
+		}
+		if v1 != v2 {
+			break
+		}
+	}
+	return false
+
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1))) // true
+	fmt.Println(Same(tree.New(1), tree.New(2))) // false
+
+}
