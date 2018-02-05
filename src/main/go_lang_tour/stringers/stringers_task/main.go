4883e151d9c446f9e641ddd73090aeedf1280a38
Mikita_Saladkevich <solodkevich@gmail.com>
2018-02-05 13:55:22 +0300

    up to date

--- src/main/go_lang_tour/stringers/stringers_task/main.go
+++ src/main/go_lang_tour/stringers/stringers_task/main.go
@@ -4,8 +4,6 @@ import "fmt"
 
 type IPAddr [4]byte
 
-// TODO: Add a "String() string" method to IPAddr.
-
 func (addr IPAddr) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", addr[0], addr[1], addr[2], addr[3])
 }
@@ -15,6 +13,7 @@ func main() {
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
+
 	for name, ip := range hosts {
 		fmt.Printf("%v: %v\n", name, ip)
 	}
